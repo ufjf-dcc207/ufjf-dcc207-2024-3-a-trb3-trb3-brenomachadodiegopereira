dbc527a6c583a04d8662e0cec36fdb5c0e106b16
Breno Machado <burracha.6464@gmail.com>
2025-03-17 17:09:05 -0300

    Adiciona github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "ufjf-dcc207-2024-3-a-trb3-trb3-brenomachadodiegopereira",
   plugins: [react()],
 })
