794aa7fdaaf5bc82f78693994c389c2ed015b561
Diego Pereira Betti <diegopbetti.gmail.com>
2025-03-17 00:44:01 -0300

    Implementa a api de pokemon e altera os componentes

--- src/Componente/Pokemon.tsx
+++ src/Componente/Pokemon.tsx
@@ -1,15 +1,40 @@
-import './Pokemon.css';
+import { useState , useEffect } from "react";
+import './Pokemon.css'
 
-export default function Pokemon() {
-  const pokemon = {
-    name: 'Pikachu', //nome fixo
-    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png', //imagem fixa
-  };
-  
+export default function Pokemon()
+{
+  const [pokemon, setPokemon ] = useState<{nome: string ; imagem: string } | null>(null);
+
+  async function pegaPokemon() {
+    try {
+      const idRandom = Math.floor(Math.random() * 151) + 1; 
+      const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${idRandom}`);  
+
+      const escolhido = await resposta.json(); 
+      
+      setPokemon({
+        nome: escolhido.name,
+        imagem: escolhido.sprites.front_default,
+      });
+    } catch (error) {
+      console.error('Erro ao buscar Pokémon:', error);
+    }
+  }
+
+  useEffect(() => {
+    pegaPokemon();
+  }, []);
+
+  if (!pokemon) {
+    return <div>Procurando...</div>;
+  }
+
   return (
-    <div className="pokemon"> 
-      <h2>{pokemon.name}</h2>
-      <img src={pokemon.image} alt={pokemon.name} />
+    <div className="pokemon">
+      <h2>{pokemon.nome}</h2>
+      <img src={pokemon.imagem} alt={pokemon.nome} />
+      <button onClick={pegaPokemon}>Novo Pokémon</button>
     </div>
   );
+
 }
