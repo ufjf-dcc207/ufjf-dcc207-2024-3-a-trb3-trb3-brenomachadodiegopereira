9443ecd703f0201ea91081c6f09ee8de8ca00f22
Breno Machado <burracha.6464@gmail.com>
2025-03-17 17:51:35 -0300

    Adiciona / faltando

--- src/Componente/Tentativas.tsx
+++ src/Componente/Tentativas.tsx
@@ -13,7 +13,7 @@ export default function Tentativas({ tentativasRestantes }: TentativasProps) {
         {pokebolas.map((_, index) => (
           <img
             key={index}
-            src="ufjf-dcc207-2024-3-a-trb3-trb3-brenomachadodiegopereira/imagens/pokebola.png" 
+            src="/ufjf-dcc207-2024-3-a-trb3-trb3-brenomachadodiegopereira/imagens/pokebola.png" 
             alt="Pokébola"
             className={`pokebola ${index < 3 - tentativasRestantes ? 'preta' : ''}`}
           />
