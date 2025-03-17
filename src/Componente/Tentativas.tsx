2f2a54ca39df13e937ceb6bb3eecfcf528a5f73b
Breno Machado <burracha.6464@gmail.com>
2025-03-17 17:49:24 -0300

    Tenta arrumar imagens

--- src/Componente/Tentativas.tsx
+++ src/Componente/Tentativas.tsx
@@ -13,7 +13,7 @@ export default function Tentativas({ tentativasRestantes }: TentativasProps) {
         {pokebolas.map((_, index) => (
           <img
             key={index}
-            src="/imagens/pokebola.png" 
+            src="ufjf-dcc207-2024-3-a-trb3-trb3-brenomachadodiegopereira/imagens/pokebola.png" 
             alt="Pokébola"
             className={`pokebola ${index < 3 - tentativasRestantes ? 'preta' : ''}`}
           />
